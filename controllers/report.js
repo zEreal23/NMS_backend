c7372256c515ccf783dfc3969ebcda5a6ca56669
Guy <kayguyx2@gmail.com>
2021-03-15 23:28:23 +0700

    update week

--- controllers/report.js
+++ controllers/report.js
@@ -1,4 +1,7 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
+const moment = require('moment');
+const _ = require('lodash');
 
 exports.reportBestSeles = async (req, res) => {
     try {
@@ -8,8 +11,8 @@ exports.reportBestSeles = async (req, res) => {
             no: index + 1,
             amount: product.sold,
             name: product.name,
-            category: product.category.name
-        }))
+            category: product.category.name,
+        }));
         return res.status(200).json(result);
     } catch (err) {
         return res.status(500).json({message: err.message});
@@ -23,8 +26,33 @@ exports.reportBadSeles = async (req, res) => {
             no: index + 1,
             amount: product.sold,
             name: product.name,
-            category: product.category.name
-        }))
+            category: product.category.name,
+        }));
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json({message: err.message});
+    }
+};
+
+exports.reportImcomeWeek = async (req, res) => {
+
+    const week = 7;
+    let result = [];
+
+    try {
+        for(let i = 0; i < week; i++) {
+            const startDay = moment().subtract(i, 'days').startOf('day')
+            const endDay = moment().subtract(i, 'days').endOf('day')
+            const order = await Order.find({updatedAt: {$gte: startDay, $lt: endDay}});
+            let sum = _.reduce(order, function(memo, reading){ return memo + reading.amount; }, 0);
+            const data = {
+                time: moment().subtract(i, 'days'),
+                day: moment().subtract(i, 'days').calendar(),
+                amount: sum
+            }
+
+            result.push(data)
+        }
         return res.status(200).json(result);
     } catch (err) {
         return res.status(500).json({message: err.message});
--- routes/report.js
+++ routes/report.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {reportBestSeles, reportBadSeles} = require('../controllers/report');
+const {reportBestSeles, reportBadSeles, reportImcomeWeek} = require('../controllers/report');
 const router = express.Router();
 
 router.get('/report/best-seles', reportBestSeles);
 router.get("/report/bad-seles" , reportBadSeles);
+router.get("/report/week" , reportImcomeWeek);
 
 module.exports = router;
