1ae8bda3eebb0aa872acbe6d697bb9b0ba3d1aaf
Guy <kayguyx2@gmail.com>
2021-03-16 00:02:22 +0700

    add month

--- controllers/report.js
+++ controllers/report.js
@@ -34,26 +34,53 @@ exports.reportBadSeles = async (req, res) => {
     }
 };
 
-exports.reportImcomeWeek = async (req, res) => {
-
+exports.reportImcomeDays = async (req, res) => {
     const week = 7;
     let result = [];
 
     try {
-        for(let i = 0; i < week; i++) {
-            const startDay = moment().subtract(i, 'days').startOf('day')
-            const endDay = moment().subtract(i, 'days').endOf('day')
+        for (let i = 0; i < week; i++) {
+            const startDay = moment().subtract(i, 'days').startOf('day');
+            const endDay = moment().subtract(i, 'days').endOf('day');
             const order = await Order.find({updatedAt: {$gte: startDay, $lt: endDay}});
-            let sum = _.reduce(order, function(memo, reading){ return memo + reading.amount; }, 0);
+            let sum = _.reduce(
+                order,
+                function (memo, reading) {
+                    return memo + reading.amount;
+                },
+                0,
+            );
             const data = {
                 time: moment().subtract(i, 'days'),
                 day: moment().subtract(i, 'days').calendar(),
-                amount: sum
-            }
+                amount: sum,
+            };
 
-            result.push(data)
+            result.push(data);
         }
         return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json({message: err.message});
+    }
+};
+
+exports.reportImcomeMonth = async (req, res) => {
+    const month = Number(moment().format('MM'));
+    let result = [];
+    try {
+        for(let i = 0; i < month; i++) {
+            const startMonth = moment().subtract(i, 'months').startOf('month');
+            const endMonth = moment().subtract(i, 'months').endOf('month');
+            const order = await Order.find({updatedAt: {$gte: startMonth, $lt: endMonth}});
+            let sum = _.reduce(order, function(memo, reading){ return memo + reading.amount; }, 0);
+            const data = {
+                time: moment().subtract(i, 'months'),
+                month: moment().subtract(i, 'months').format('MMMM'),
+                amount: sum
+            }
+            result.push(data)
+        }
+        return res.status(200).json({data: result});
     } catch (err) {
         return res.status(500).json({message: err.message});
     }
--- routes/report.js
+++ routes/report.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {reportBestSeles, reportBadSeles, reportImcomeWeek} = require('../controllers/report');
+const {reportBestSeles, reportBadSeles, reportImcomeDays, reportImcomeMonth} = require('../controllers/report');
 const router = express.Router();
 
 router.get('/report/best-seles', reportBestSeles);
 router.get("/report/bad-seles" , reportBadSeles);
-router.get("/report/week" , reportImcomeWeek);
+router.get("/report/days" , reportImcomeDays);
+router.get("/report/months" , reportImcomeMonth);
 
 module.exports = router;
